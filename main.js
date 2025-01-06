213c3631c379858f5fb3fd6ed5531c43c5ec7290
MaelBarret <90317751+MaelBarret@users.noreply.github.com>
2025-01-06 17:22:15 +0100

    OTM

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const map = new L.map('map').setView([43.58,3.367], 10);
 
 var basemaps = {
 	OSM : L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png"),
-	OpenTopoMap : L.tileLayer("http://tile.opentopomap.org/{z}/{x}/{y}.png"),
+	OpenTopoMap : L.tileLayer("http://a.tile.opentopomap.org/{z}/{x}/{y}.png"),
 };
 
 L.control.layers(basemaps).addTo(map);
